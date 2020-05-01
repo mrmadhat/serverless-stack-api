eaf5642cf8a0bdf0fabf7fb1cfbc661fee489322
mrmadhat <daniel@relativemarketing.co.uk>
2020-05-01 14:52:25 +0100

    Adding error logging

---
+++ libs/debug-lib.js
@@ -0,0 +1,39 @@
+import AWS from "aws-sdk"
+import util from "util"
+
+AWS.config.logger = { log: debug }
+
+let logs
+let timeoutTimer
+
+export function init(event, context) {
+  logs = []
+
+  debug("API event", {
+    body: event.body,
+    pathParameters: event.pathParameters,
+    queryStringParameters: event.queryStringParameters,
+  })
+
+  // Start timeout timer
+  timeoutTimer = setTimeout(() => {
+    timeoutTimer && flush(new Error("Lambda will timeout in 100ms"))
+  }, context.getRemainingTimeInMillis() - 100)
+}
+
+export function end() {
+  clearTimeout(timeoutTimer)
+  timeoutTimer = null
+}
+
+export function flush(e) {
+  logs.forEach(({ date, string }) => console.debug(data, string))
+  console.error(e)
+}
+
+export default function debug() {
+  logs.push({
+    date: new Date(),
+    string: util.format.apply(null, arguments),
+  })
+}
--- libs/handler-lib.js
+++ libs/handler-lib.js
@@ -1,11 +1,16 @@
+import * as debug from "./debug-lib"
+
 export default function handler(lambda) {
   return function (event, context) {
     return (
       Promise.resolve()
+        // Start debugger
+        .then(() => debug.init(event, context))
         // run the lambda
         .then(() => lambda(event, context))
         .then((responseBody) => [200, responseBody])
         .catch((e) => {
+          debug.flush(e)
           return [500, { error: e.message }]
         })
         .then(([statusCode, body]) => ({
@@ -16,6 +21,7 @@ export default function handler(lambda) {
           },
           body: JSON.stringify(body),
         }))
+        .finally(debug.end)
     )
   }
 }
